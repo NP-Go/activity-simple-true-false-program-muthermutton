a7d29e79b2a9702edecffe0af21069dd80091194
muthermutton <khairal.fazil@gmail.com>
2022-03-29 10:54:53 +0800

    added PrintLn to conditions

--- trueFalse.go
+++ trueFalse.go
@@ -10,10 +10,13 @@ func compare(value int) string {
 	//Insert your code from here
 	if value == secretValue {
 		resultMessge = "Well Done! Your guess is correct"
+		fmt.Println(resultMessge)
 	} else if value < secretValue {
 		resultMessge = "Too low, try again next time!"
+		fmt.Println(resultMessge)
 	} else {
 		resultMessge = "Too high, try again next time!"
+		fmt.Println(resultMessge)
 	}
 	//do not remove this line
 	return resultMessge
